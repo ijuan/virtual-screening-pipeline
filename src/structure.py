3d88a4ea7935917cda2fe4c51fd9c4207c5140c1
ian <iancta29@gmail.com>
2026-08-23 17:05:01 -0700

    Added to parse_hetatm to parse all ligand entries rather than just on based on residue_name

--- src/structure.py
+++ src/structure.py
@@ -28,10 +28,13 @@ def parse_hetatms(pdb_path) -> dict:
     for line in pdb_line:
         if line.startswith("HETATM"):
             residue_name = line[17:20].strip()
+            chain        = line[21]
+            res_num      = line[22:26].strip()
+            key = (residue_name, chain, res_num)
             x = float(line[30:38])
             y = float(line[38:46])
             z = float(line[46:54])
-            parsed_hetatm.setdefault(residue_name,[]).append((x, y, z))
+            parsed_hetatm.setdefault(key, []).append((x, y, z))
     return parsed_hetatm
 
 # Set of residue names that aren't in the ligand
@@ -63,13 +66,29 @@ def pick_ligand(hetatm: dict, min_atoms=10, ligand=None) -> str:
             best = name
     return best
 
-        
 
-    
+def compute_box(coords:list, padding=8.0) -> list:
+    x_coords = [c[0] for c in coords]
+    y_coords = [c[1] for c in coords]
+    z_coords = [c[2] for c in coords]
+
+    x_avg = sum(x_coords) / len(x_coords)
+    y_avg = sum(y_coords) / len(y_coords)
+    z_avg = sum(z_coords) / len(z_coords)
+    center = (x_avg, y_avg, z_avg)
+
+    x_size = (max(x_coords) - min(x_coords)) + 2 * padding
+    y_size = (max(y_coords) - min(y_coords)) + 2 * padding
+    z_size = (max(z_coords) - min(z_coords)) + 2 * padding
+    size = ( x_size, y_size, z_size)
+
+    return [center, size]
 
 
 
 
 
 
-
+if __name__ == "__main__":
+   x = compute_box([(0.639, 22.832, 7.09), (1.119, 22.308, 8.239), (0.248, 22.065, 9.241), (2.415, 22.022, 8.41), (3.34, 22.233, 7.433)])
+   print(x)
